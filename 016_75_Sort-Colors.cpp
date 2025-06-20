fe909216e784cc7b4fa072053df23c53444336b3
Abhirup Ghosh <codewithabhi2024@gmail.com>
2025-06-20 16:36:40 +0530

    add problem 75

--- 016_75_Sort-Colors.cpp
+++ 016_75_Sort-Colors.cpp
@@ -1,7 +1,7 @@
 //https://leetcode.com/problems/sort-colors/description/
 
-//* Approach 1 -> Better  
-//* Approach 2 -> Optimal - Dutch National Flag Algorithm
+//* Approach 1 -> Better  o(n^2) o(1)
+//* Approach 2 -> Optimal - Dutch National Flag Algorithm o(n) o(1)
 #include <iostream>
 #include <vector>
 
@@ -23,5 +23,30 @@ public:
         for(i = 0; i < count_zero; ++i) nums[i] = 0;
         for(i = count_zero; i < count_zero + count_one;++i) nums[i] = 1;
         for(i = count_zero + count_one; i < nums.size(); ++i) nums[i] = 2;
+    }
+};
+
+//*Dutch National Flag Algorithm
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                ++low;
+                ++mid;
+            } else if (nums[mid] == 1) {
+                ++mid;
+            } else {
+                swap(nums[mid], nums[high]);
+                --high; 
+            }
+        }
     }
 };

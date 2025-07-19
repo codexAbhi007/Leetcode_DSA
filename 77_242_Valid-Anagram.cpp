1b20afeaf9eac6ad37534f3d9f6cf896b170955c
Abhirup Ghosh <codewithabhi2024@gmail.com>
2025-07-19 23:10:24 +0530

    242

---
+++ 77_242_Valid-Anagram.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/valid-anagram/description/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> count(26, 0);
+
+        for (char c : s) {
+            count[c - 'a'] += 1;
+        }
+
+        for (char c : t) {
+            if (count[c - 'a'] == 0) {
+                return false;
+            }
+            count[c - 'a'] -= 1;
+        }
+
+        return true;        
+    }
+};

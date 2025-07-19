1acd8d3cf0cf7c99bff6e3734ff711f076c588af
Abhirup Ghosh <codewithabhi2024@gmail.com>
2025-07-19 23:09:52 +0530

    1021

---
+++ 71_1021_Remove_Outer_Parentheses.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/remove-outermost-parentheses/description/
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        int balance = 0;
+
+        for(int i = 0; i < s.size(); ++i){
+            if(s[i] == '('){
+                if(balance > 0) result += s[i];
+                balance++;
+            }else{
+                balance--;
+                if(balance > 0){
+                    result += s[i];
+                }
+            }
+        }
+        return result;
+    }
+};

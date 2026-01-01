db89641924b7351a945a7361c97a3dc34794fab6
Abhirup Ghosh <codewithabhi2024@gmail.com>
2026-01-01 22:57:19 +0530

    add 66

---
+++ 102_66_Plus-One.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/plus-one/?envType=daily-question&envId=2026-01-01
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10;
+            carry = sum / 10;
+        }
+        if (carry) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};

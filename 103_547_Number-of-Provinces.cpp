923d75ba8cc9fd35a22d96fee3e294b4c9f5bce6
Abhirup Ghosh <codewithabhi2024@gmail.com>
2026-01-04 10:23:47 +0530

    add 547

---
+++ 103_547_Number-of-Provinces.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/number-of-provinces/
+
+class Solution {
+public:
+    void dfs(int city, vector<vector<int>>& isConnected, vector<int>& vis) {
+        vis[city] = 1;
+
+        for (int j = 0; j < isConnected.size(); j++) {
+            if (isConnected[city][j] == 1 && !vis[j]) {
+                dfs(j, isConnected, vis);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                provinces++;
+                dfs(i, isConnected, vis);
+            }
+        }
+        return provinces;
+    }
+};

ba183a9feedb1e3d14e065396deb4d3a0f8f34ef
Abhirup Ghosh <codewithabhi2024@gmail.com>
2026-01-05 19:54:09 +0530

    add 542

---
+++ 106_542_Matrix01.cpp
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/01-matrix/description/
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        vector<vector<int>> dist(m, vector<int>(n, 0));
+
+        queue<pair<pair<int,int>, int>> q;
+
+        // Step 1: Push all 0s
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(mat[i][j] == 0) {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        int delRow[4] = {-1, 0, 1, 0};
+        int delCol[4] = {0, 1, 0, -1};
+
+        // Step 2: BFS
+        while(!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+
+            dist[row][col] = steps;
+
+            for(int d = 0; d < 4; d++) {
+                int nrow = row + delRow[d];
+                int ncol = col + delCol[d];
+
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n
+                   && vis[nrow][ncol] == 0) {
+
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, steps + 1});
+                }
+            }
+        }
+
+        return dist;
+    }
+};
